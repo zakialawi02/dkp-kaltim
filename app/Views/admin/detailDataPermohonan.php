d08b857cddf55ee8f58d4585d237b7bdd3de75e4
zakialawi02 <zaki.alawi22@gmail.com>
2023-10-15 16:59:30 +0700

    fix basemap layerSwitcher with ol-ext

--- app/Views/admin/detailDataPermohonan.php
+++ app/Views/admin/detailDataPermohonan.php
@@ -425,7 +425,7 @@
             source: vectorSource,
             style: styleDraw,
             name: 'Data Pemohon',
-            zIndex: 1
+            zIndex: 5
         });
         const projection = new ol.proj.Projection({
             code: 'EPSG:54034',
@@ -440,6 +440,7 @@
             source: new ol.source.OSM(),
             crossOrigin: 'anonymous',
             visible: true,
+            baseLayer: true,
         });
 
         const sourceBingMaps = new ol.source.BingMaps({
@@ -454,6 +455,7 @@
             source: sourceBingMaps,
             crossOrigin: 'anonymous',
             visible: false,
+            baseLayer: true,
         });
 
         const mapboxBaseURL = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiNjg2MzUzMyIsImEiOiJjbDh4NDExZW0wMXZsM3ZwODR1eDB0ajY0In0.6jHWxwN6YfLftuCFHaa1zw';
@@ -467,9 +469,16 @@
             visible: false,
             source: mapboxSource,
             crossOrigin: 'anonymous',
+            baseLayer: true,
         });
 
-        const baseMaps = [osmBaseMap, bingAerialBaseMap, mapboxBaseMap];
+        const baseMaps = new ol.layer.Group({
+            title: 'Base Layers',
+            openInLayerSwitcher: true,
+            layers: [
+                osmBaseMap, bingAerialBaseMap, mapboxBaseMap
+            ]
+        });
 
         // Init To Canvas/View
         const view = new ol.View({
@@ -665,8 +674,8 @@
                             return lineStyleEks;
                         }
                     },
-                    name: 'Semua Data Telah Disetujui',
-                    zIndex: 2
+                    name: 'Data Telah Disetujui',
+                    zIndex: 1
                 });
 
                 map.addLayer(vectorLayerEks);
